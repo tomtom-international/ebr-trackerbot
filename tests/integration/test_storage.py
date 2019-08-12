bd5356bccc60e6771c5f148f2f37efb9835389fc
Milan Hradil <milan.hradil@tomtom.com>
2019-08-12 12:35:18 +0200

    Parameterize test and add setup and teardown

--- tests/integration/test_storage.py
+++ tests/integration/test_storage.py
@@ -17,48 +17,47 @@ def delete_fixtures():
         os.unlink(tempfile.gettempdir() + "/data-odbc.db")
 
 
-def reset_fixtures():
+def setup_module():
+    bot.config["odbc_connection_string"] = "DRIVER={SQLITE3};DATABASE=" + tempfile.gettempdir() + "/data-odbc.db"
     delete_fixtures()
     open(tempfile.gettempdir() + "/data-odbc.db", "a").close()
 
 
-def test_storage():
-    reset_fixtures()
-    bot.config["odbc_connection_string"] = "DRIVER={SQLITE3};DATABASE=" + tempfile.gettempdir() + "/data-odbc.db"
-    modules = ["odbc", "sqlite"]
-
-    for module_name in modules:
-        module = None
-        for storage in bot.STATE.bot_storage:
-            if storage["name"] == module_name:
-                module = storage
-                break
-        assert len(module["load_all_tracked_tests"]()) == 0
-        module["save"](
-            "test-user", {"test": "abc", "expiry": "2100-01-01T00:00:00Z", "channel_id": "test", "thread_ts": "0123"}
-        )
-        assert len(module["load_all_tracked_tests"]()) == 1
-        assert len(module["load_for_user"]("test-user")) == 1
-        assert len(module["load_for_user"]("non-existing-user")) == 0
-        module["delete_for_user"]("test-user", "abc")
-        assert len(module["load_all_tracked_tests"]()) == 0
-        assert len(module["load_for_user"]("test-user")) == 0
-        module["save"](
-            "test-user", {"test": "abc", "expiry": "2000-01-01T00:00:00Z", "channel_id": "test", "thread_ts": "0123"}
-        )
-        module["clean_expired_tracks"]()
-        assert len(module["load_all_tracked_tests"]()) == 0
-        assert len(module["load_for_user"]("test-user")) == 0
-        module["save"](
-            "test-user", {"test": "abc", "expiry": "2000-01-01T00:00:00Z", "channel_id": "test", "thread_ts": "0123"}
-        )
-        module["save"](
-            "test-user", {"test": "abc", "expiry": "2100-01-01T00:00:00Z", "channel_id": "test2", "thread_ts": "01234"}
-        )
-        assert module["load_for_user"]("test-user")[0]["expiry"] == "2100-01-01T00:00:00Z"
-        module["save"](
-            "test-user", {"test": "abc2", "expiry": "2100-01-01T00:00:00Z", "channel_id": "test2", "thread_ts": "01234"}
-        )
-        assert len(module["load_for_user"]("test-user")) == 2
-
+def teardown_module():
     delete_fixtures()
+
+
+@pytest.mark.parametrize("module_name", ["odbc", "sqlite"])
+def test_storage(module_name):
+    module = None
+    for storage in bot.STATE.bot_storage:
+        if storage["name"] == module_name:
+            module = storage
+            break
+    assert len(module["load_all_tracked_tests"]()) == 0
+    module["save"](
+        "test-user", {"test": "abc", "expiry": "2100-01-01T00:00:00Z", "channel_id": "test", "thread_ts": "0123"}
+    )
+    assert len(module["load_all_tracked_tests"]()) == 1
+    assert len(module["load_for_user"]("test-user")) == 1
+    assert len(module["load_for_user"]("non-existing-user")) == 0
+    module["delete_for_user"]("test-user", "abc")
+    assert len(module["load_all_tracked_tests"]()) == 0
+    assert len(module["load_for_user"]("test-user")) == 0
+    module["save"](
+        "test-user", {"test": "abc", "expiry": "2000-01-01T00:00:00Z", "channel_id": "test", "thread_ts": "0123"}
+    )
+    module["clean_expired_tracks"]()
+    assert len(module["load_all_tracked_tests"]()) == 0
+    assert len(module["load_for_user"]("test-user")) == 0
+    module["save"](
+        "test-user", {"test": "abc", "expiry": "2000-01-01T00:00:00Z", "channel_id": "test", "thread_ts": "0123"}
+    )
+    module["save"](
+        "test-user", {"test": "abc", "expiry": "2100-01-01T00:00:00Z", "channel_id": "test2", "thread_ts": "01234"}
+    )
+    assert module["load_for_user"]("test-user")[0]["expiry"] == "2100-01-01T00:00:00Z"
+    module["save"](
+        "test-user", {"test": "abc2", "expiry": "2100-01-01T00:00:00Z", "channel_id": "test2", "thread_ts": "01234"}
+    )
+    assert len(module["load_for_user"]("test-user")) == 2
